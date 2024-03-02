5885dc0d59f18ffa282dc372f22d92186dfd6584
developer <1816579@gmail.com>
2024-03-02 16:46:50 +0200

    del: unused imports

--- src/controller/workers/estimationWorker.ts
+++ src/controller/workers/estimationWorker.ts
@@ -1,8 +1,5 @@
 import { WorkerInputData, convertInputData } from '@/helpers/convertWorkerData';
 import { estimate } from '../estimate';
-import { Board } from '@/model/board';
-import { Figure } from '@/model/figure';
-import { Color, Move, Position, Role, SpaceMatrix } from '@/types';
 
 addEventListener('message', (event: MessageEvent<WorkerInputData>) => {
   const convertedArgs = convertInputData(event.data);
