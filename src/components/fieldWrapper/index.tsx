501b072ab0e9ec5daff67b70fc93cb26fd731ce5
developer <1816579@gmail.com>
2024-03-17 12:59:18 +0200

    add: row numbers

--- src/components/fieldWrapper/index.tsx
+++ src/components/fieldWrapper/index.tsx
@@ -1,6 +1,24 @@
 import { ReactNode } from 'react';
 import './fieldWrapper.scss';
 
-export function FieldWrapper({children}:{children: ReactNode}){
-    return <div className='field-wrapper'>{children}</div>
+export function FieldWrapper({ children }: { children: ReactNode }) {
+  return (
+    <div className="field-wrapper">
+      <div className="row-numbers left">
+        {new Array(8).fill(true).map((_, index) => (
+          <div key={index} className="row-num">
+            {8 - index}
+          </div>
+        ))}
+      </div>
+      <div className="field-inner-wrapper">{children}</div>
+      <div className="row-numbers right">
+        {new Array(8).fill(true).map((_, index) => (
+          <div key={index} className="row-num">
+            {8 - index}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
