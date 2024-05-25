38a6a815626eee4fb9a8420e4ea99fdcaa636cbe
developer <1816579@gmail.com>
2024-05-25 22:16:37 +0300

    fix: types

--- src/helpers/levelBuilder/index.ts
+++ src/helpers/levelBuilder/index.ts
@@ -1,16 +1,23 @@
 import { Level, LevelConcept } from '@/types';
 import { createComplexCondition } from './conditionFunctions';
+import { Figure } from '@/model/figure';
 
 export function createLevel(concept: LevelConcept): Level {
   const {
+    id,
     boards,
-    figures,
+    staticFigures,
     objectives,
     allowSinglePlayer,
     accomplishedChecks: ac,
     failedChecks: fc,
   } = concept;
+
+  const figures = staticFigures.map((sf) => new Figure({ ...sf, onEvent: undefined }));
+
+  // TODO: return default checks if no checks added
   return {
+    id,
     boards,
     figures,
     objectives,
