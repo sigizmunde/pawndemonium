bcf04d0e5985389bc66741887cc37d49b0858a50
sigizmunde <1816579@gmail.com>
2025-09-13 23:25:46 +0300

    upd: objectives frame appearance

--- src/components/objectives/index.tsx
+++ src/components/objectives/index.tsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from 'react';
 import './objectives.scss';
 
-export function Objectives({ info = '' }: { info?: string }) {
+export function Objectives({
+  info = '',
+  showByDefault = true,
+}: {
+  info?: string;
+  showByDefault?: boolean;
+}) {
   const [mobileObjShow, setMobileObjShow] = useState(false);
 
   useEffect(() => {
-    setMobileObjShow(true);
-  }, [info])
+    setMobileObjShow(showByDefault);
+  }, [info, showByDefault]);
 
   const contents = info.split('\n').map((p, i) => <p key={i}>{p}</p>);
 
   return info ? (
     <>
-      <div className="objectives">{contents}</div>
+      <div className="objectives">
+        <div className="objectives-contents">{contents}</div>
+      </div>
       <div className={`objectives-mobile ${mobileObjShow ? 'open' : 'closed'}`}>
         <button onClick={() => setMobileObjShow((state) => !state)}>
           {mobileObjShow ? 'X' : '<'}
         </button>
-        {contents}
+        <div className="objectives-contents">{contents}</div>
       </div>
     </>
   ) : null;
